e063ccd288bbb04dcfd6658d26a3b27fa02fa207
suryaAmmisetti <pavidipallas@gmail.com>
2022-10-22 17:25:52 +0530

    HelloController-Added

---
+++ src/main/java/com/awssurya/suryaaws/HelloController.java
@@ -0,0 +1,12 @@
+package com.awssurya.suryaaws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Surya Ammisetti application for AWS GITHUB CI/CD";
+    }
+}
