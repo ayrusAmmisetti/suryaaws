0fb083b058bd47deedddd7331ca24ce2f78ca196
suryaAmmisetti <pavidipallas@gmail.com>
2022-10-22 17:24:03 +0530

    firstCommit

---
+++ src/main/java/com/awssurya/suryaaws/SuryaawsApplication.java
@@ -0,0 +1,13 @@
+package com.awssurya.suryaaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuryaawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuryaawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awssurya/suryaaws/SuryaawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awssurya.suryaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuryaawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
